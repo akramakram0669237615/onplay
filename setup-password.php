e7ed71b67046fda2e229bbf5b085a4ef113fb30c
akramakram0669237615 <kaidakram215@gmail.com>
2026-08-28 16:14:52 +0100

    Update password setup instructions in setup-password.php

--- setup-password.php
+++ setup-password.php
@@ -1,6 +1,6 @@
 <?php
 // احذف هذا الملف بعد الاستخدام!
-if(!isset($_GET['password']))exit('أضف ?password=YOUR_STRONG_PASSWORD');
+if(!isset($_GET['password']))exit('أضف ?password=NemoWeb@2026');
 require __DIR__.'/src/bootstrap.php';
 $u=db_read('users');$u[0]['password']=password_hash((string)$_GET['password'],PASSWORD_DEFAULT);db_write('users',$u);
 echo 'تم إنشاء كلمة المرور. احذف setup-password.php الآن.';
